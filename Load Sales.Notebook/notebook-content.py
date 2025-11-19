c28c871e7e2b3d7722df75ffd5e79ec3751c1c46
Derek Pike <38468752+Derekp20@users.noreply.github.com>
2025-11-19 19:23:31 +0000

    Committing 18 items from workspace b99b4aa1-6a08-45b1-bb97-87a06587f0da

---
+++ Load Sales.Notebook/notebook-content.py
@@ -0,0 +1,95 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "dd09c761-edf6-4f0f-bf2e-0e2b48d12917",
+# META       "default_lakehouse_name": "LH01",
+# META       "default_lakehouse_workspace_id": "b99b4aa1-6a08-45b1-bb97-87a06587f0da",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "dd09c761-edf6-4f0f-bf2e-0e2b48d12917"
+# META         },
+# META         {
+# META           "id": "4c2982e3-11a3-4695-a132-34b27f613665"
+# META         }
+# META       ]
+# META     },
+# META     "environment": {}
+# META   }
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# PARAMETERS CELL ********************
+
+table_name = "sales"
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+
+# Read the new sales data
+df = spark.read.format("csv").option("header","true").load("Files/new_data/*.csv")
+
+## Add month and year columns
+df = df.withColumn("Year", year(col("OrderDate"))).withColumn("Month", month(col("OrderDate")))
+
+# Derive FirstName and LastName columns
+df = df.withColumn("FirstName", split(col("CustomerName"), " ").getItem(0)).withColumn("LastName", split(col("CustomerName"), " ").getItem(1))
+
+# Filter and reorder columns
+df = df["SalesOrderNumber", "SalesOrderLineNumber", "OrderDate", "Year", "Month", "FirstName", "LastName", "EmailAddress", "Item", "Quantity", "UnitPrice", "TaxAmount"]
+
+# Load the data into a table
+df.write.format("delta").mode("append").saveAsTable(table_name)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+abfss://Demo@onelake.dfs.fabric.microsoft.com/LH02.Lakehouse/Files
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC SELECT * FROM sales;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
